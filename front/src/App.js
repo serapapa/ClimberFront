155620e22f4c10abf8b459f524eda1aec938b65e
serapapa <111412101+serapapa@users.noreply.github.com>
2022-12-13 21:18:36 +0900

    add component
    add component

--- front/src/App.js
+++ front/src/App.js
@@ -1,32 +1,25 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Headers from './component/Headers'
+import Nav from './component/Nav'
 import Community from './pages/Community'
 import Club from './pages/Club'
 import Center from './pages/Center'
 import My from './pages/My'
-import Nav from './pages/Nav'
-import Headers from './pages/Headers'
-import Articles from './pages/Articles'
-
 
 function App() {
   return (
     <>
-    {/* 헤더영역, 각 페이지 연결하는 버튼만 들어가는 공간 */}
-    <Headers></Headers>
-    {/* 섹션영역, 아티클 형태로 객체가 유입되는 공간 */}
-    <section>
-      <h1>커뮤니티홈</h1>
-      <Articles/>
-    </section>
-    <Nav/>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Community />} />
-        <Route path="/Club" element={<Club />} />
-        <Route path="/Center" element={<Center />} />
-        <Route path="/My" element={<My />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Headers></Headers>
+        <Routes>
+          <Route path="/" element={<Community />} />
+          <Route path="/Club" element={<Club />} />
+          <Route path="/Center" element={<Center />} />
+          <Route path="/My" element={<My />} />
+          <Route path="/Community/write" element={<My />} />
+        </Routes>
+        <Nav></Nav>
+      </BrowserRouter>
     </>
   )
 }
